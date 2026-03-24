931fed5563a7e3d07ee2f049479eb77c03134da4
nmquan1503 <minhquan15032005@gmail.com>
2026-03-24 21:05:12 +0700

    feat: add building dataloader script

---
+++ data/dataloader.py
@@ -0,0 +1,42 @@
+import torch
+from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
+from functools import partial
+
+from .dataset import MTDataset
+from .tokenizer import Tokenizer
+import config
+
+def collate_fn(batch, pad_id):
+    input_ids = [item["input_ids"] for item in batch]
+    target_ids = [item["target_ids"] for item in batch]
+
+    input_lengths = torch.tensor([ip.size(0) for ip in input_ids], dtype=torch.long)
+    input_ids = pad_sequence(input_ids, batch_first=True, padding_value=pad_id)
+    target_ids = pad_sequence(target_ids, batch_first=True, padding_value=pad_id)
+
+    return {
+        "input_ids": input_ids,
+        "input_lengths": input_lengths,
+        "target_ids": target_ids
+    }
+
+def build_dataloader(
+    src_text_path: str, 
+    tgt_text_path: str, 
+    tokenizer: Tokenizer,
+    shuffle=True
+):
+    dataset = MTDataset(src_text_path, tgt_text_path, tokenizer)
+    collate = partial(collate_fn, pad_id=tokenizer.pad_id)
+    return DataLoader(
+        dataset,
+        batch_size=config.BATCH_SIZE,
+        shuffle=shuffle,
+        num_workers=4,
+        pin_memory=True,
+        persistent_workers=True,
+        prefetch_factor=4,
+        collate_fn=collate
+    )
+
