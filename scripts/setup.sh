8bc75522067ccca4c486c38da565a4d7491a6758
nmquan1503 <minhquan15032005@gmail.com>
2026-03-24 22:35:28 +0700

    add bash scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,16 @@
+set -e
+
+echo "Cloning Mamba..."
+git clone https://github.com/nmquan1503/ssm-mamba.git ssm_mamba -q
+
+cd ssm_mamba
+
+echo "Installing Mamba..."
+pip install . --no-build-isolation -q
+
+cd ..
+
+echo "Installing required libs..."
+pip install sacrebleu -q
+
+echo "Done."
