98a27b85c11b128c77c553674806a205a7b67cbc
nmquan1503 <minhquan15032005@gmail.com>
2026-04-01 02:20:16 +0700

    fix setup causal-conv1d

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,7 +14,7 @@ cd ..
 
 echo "Installing Causal-conv1d..."
 git clone https://github.com/Dao-AILab/causal-conv1d.git -q
-cd causal_conv1d
+cd causal-conv1d
 pip install . --no-build-isolation -q
 cd ..
 
