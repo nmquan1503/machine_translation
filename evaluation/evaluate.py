6a121b9ead2309b639b19141d533acc61f339cb3
nmquan1503 <minhquan15032005@gmail.com>
2026-04-05 13:27:06 +0700

    feat: add mamba3

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -12,23 +12,43 @@ def evaluate():
     tokenizer = Tokenizer()
     test_loader = build_dataloader(config.TEST_SRC_PATH, config.TEST_TGT_PATH, tokenizer, False, mode="test")
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    ssm_cfg={
+        "layer": config.TYPE,
+        "d_state": config.STATE_DIM,
+        "d_conv": config.CONV_KERNEL,
+    }
+    if config.TYPE == "Mamba1":
+        ssm_cfg["use_fast_path"] = False
+        ssm_cfg["conv_bias"] = True
+        ssm_cfg["bias"] = True
+    elif config.TYPE == "Mamba2":
+        ssm_cfg["use_mem_eff_path"] = False
+        ssm_cfg["sequence_parallel"] = False
+        ssm_cfg["chunk_size"] = config.CHUNK_SIZE
+        ssm_cfg["headdim"] = config.MODEL_DIM * 2 / config.NUM_HEADS
+        ssm_cfg["bias"] = True
+        ssm_cfg["conv_bias"] = True
+        ssm_cfg["ngroups"] = config.NUM_GROUPS
+    elif config.TYPE == "Mamba3":
+        ssm_cfg["mimo"] = True
+        ssm_cfg["mimo_rank"] = config.MIMO_RANK
+        ssm_cfg["chunk_size"] = 64 // config.MIMO_RANK
+        ssm_cfg["headdim"] = config.MODEL_DIM * 2 / config.NUM_HEADS
+        ssm_cfg["ngroups"] = config.NUM_GROUPS
     model = MambaLMHeadModel(MambaConfig(
         d_model=config.MODEL_DIM,
         n_layer=config.NUM_LAYERS,
         d_intermediate=config.INNER_DIM,
         vocab_size=config.VOCAB_SIZE,
-        ssm_cfg={
-            "layer": config.TYPE,
-            "d_state": config.STATE_DIM,
-            "d_conv": config.CONV_KERNEL,
-            "use_mem_eff_path" if config.TYPE == "Mamba2" else "use_fast_path": False
-        },
+        ssm_cfg=ssm_cfg,
         attn_layer_idx=config.ATTENTION_LAYERS,
         attn_cfg={
-            "num_heads": config.NUM_HEADS
+            "num_heads": config.NUM_HEADS,
+            "causal": True,
+            "embed_dim": config.MODEL_DIM
         },
-        rms_norm=config.USE_RMS_NORM, 
-        fused_add_norm=config.USE_FUSE_ADD_NORM,
+        rms_norm=True, 
+        fused_add_norm=True,
         residual_in_fp32=True,
         tie_embeddings=True,
         pad_vocab_size_multiple=1
@@ -53,7 +73,7 @@ def evaluate():
                 seq_ids = model.generate(single_input, max_length=config.MAX_NEW_TOKENS, eos_token_id=tokenizer.eos_id, cg=True).cpu()
 
             pred = seq_ids[0].tolist()
-            pred = pred[pred.index(tokenizer.bos_id)]
+            pred = pred[pred.index(tokenizer.bos_id):]
 
             pred_text = tokenizer.decode(pred)
             tgt_text = tokenizer.decode(single_output)
