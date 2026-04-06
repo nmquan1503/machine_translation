874e625ebaa6ef896b9cef5c5dab667ebe0ace16
nmquan1503 <minhquan15032005@gmail.com>
2026-04-06 21:33:20 +0700

    fix mamba3

--- mamba/mamba_ssm/modules/mamba3.py
+++ mamba/mamba_ssm/modules/mamba3.py
@@ -84,14 +84,8 @@ class Mamba3(nn.Module):
         assert self.num_rope_angles > 0
 
         # Order: [z, x, B, C, dd_dt, dd_A, trap, angle]
-        d_in_proj = 2 * self.d_inner + 2 * self.d_state * self.num_bc_heads * self.mimo_rank + 3 * self.nheads + self.num_rope_angles
-        try:
-            self.in_proj = nn.Linear(self.d_model, d_in_proj, bias=False, **factory_kwargs)
-        except:
-            print("ok")
-            print(self.d_model)
-            print(d_in_proj)
-            print("okkkkk")
+        d_in_proj = int(2 * self.d_inner + 2 * self.d_state * self.num_bc_heads * self.mimo_rank + 3 * self.nheads + self.num_rope_angles)
+        self.in_proj = nn.Linear(self.d_model, d_in_proj, bias=False, **factory_kwargs)
 
         # dt_bias parameterization        
         _dt = torch.exp(
